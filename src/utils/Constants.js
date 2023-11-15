03e2a1f419016ef1b4ac24d1476cba169a7bc046
kimsuyeon0916 <kimsuyeon_09162naver.com>
2023-11-15 17:20:01 +0900

    fix: 상수 메시지 개행 문자 수정

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -5,7 +5,7 @@ const MESSAGE = Object.freeze({
   getMenu:
     "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n",
   preview: "우테코 식당에서 받을 이벤트 혜택 미리 보기!\n",
-  menu: "\n<주문 메뉴>",
+  menu: "<주문 메뉴>",
   cost: "\n<할인 전 총주문 금액>\n",
   giveaway: "\n<증정 메뉴>\n",
   benefit: "\n<혜택 내역>",
