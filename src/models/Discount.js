18e1105c8fef5acc7acde66e252360dbce2828c0
kimsuyeon0916 <kimsuyeon_09162naver.com>
2023-11-15 14:49:09 +0900

    feat: Discount 구현
    - calculateTotalDiscount
    - calculateDiscount
    - #christmasDiscount
    - #weekdayDiscount
    -#weekendDiscount
    -#specialDiscount

--- src/models/Discount.js
+++ src/models/Discount.js
@@ -1,3 +1,5 @@
+import { DATE, DISCOUNT, MENU } from "../utils/Constants.js";
+
 class Discount {
   #discount;
 
@@ -6,29 +8,57 @@ class Discount {
   }
 
   calculateTotalDiscount() {
-    
+    return Array.from(this.#discount.values()).reduce(
+      (total, cost) => total + cost,
+      0
+    );
   }
 
   calculateDiscount(date, order) {
-    
+    this.#christmasDiscount(date);
+    this.#weekdayDiscount(date, order);
+    this.#weekendDiscount(date, order);
+    this.#specialDiscount(date);
 
     return this.#discount;
   }
 
   #christmasDiscount(date) {
-    
+    if (date <= DATE.christmas) {
+      const amount = DISCOUNT.unit + (date - 1) * DISCOUNT.increase;
+
+      this.#discount.set("크리스마스 디데이 할인", amount);
+    }
   }
 
   #weekdayDiscount(date, order) {
-    
+    if (DATE.weekday.includes(date)) {
+      const dessert = Array.from(MENU.get("디저트").keys());
+      const dessertOrder = Array.from(order).filter(([menu]) =>
+        dessert.includes(menu)
+      );
+      const count = dessertOrder.reduce((sum, [, number]) => sum + number, 0);
+
+      this.#discount.set("평일 할인", count * DISCOUNT.year);
+    }
   }
 
   #weekendDiscount(date, order) {
-    
+    if (DATE.weekend.includes(date)) {
+      const main = Array.from(MENU.get("메인").keys());
+      const mainOrder = Array.from(order).filter(([menu]) =>
+        main.includes(menu)
+      );
+      const count = mainOrder.reduce((sum, [, number]) => sum + number, 0);
+
+      this.#discount.set("주말 할인", count * DISCOUNT.year);
+    }
   }
 
   #specialDiscount(date) {
-    
+    if (DATE.special.includes(date)) {
+      this.#discount.set("특별 할인", DISCOUNT.unit);
+    }
   }
 }
 
