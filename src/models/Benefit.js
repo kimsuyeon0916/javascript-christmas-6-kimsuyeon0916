6404067a9291a728806e7bf3a3714f757ba3e751
kimsuyeon0916 <kimsuyeon_09162naver.com>
2023-11-15 14:51:38 +0900

    feat: Benefit 구현
    - calculateTotalBenefit
    - calculateBenefit
    - calculateGiveaway

--- src/models/Benefit.js
+++ src/models/Benefit.js
@@ -1,3 +1,5 @@
+import { COST_CRITERIA } from "../utils/Constants.js";
+
 class Benefit {
   #benefit;
 
@@ -8,16 +10,26 @@ class Benefit {
   }
 
   calculateTotalBenefit() {
-    
+    return Array.from(this.#benefit.values()).reduce(
+      (total, cost) => total + cost,
+      0
+    );
   }
 
   calculateBenefit(discount, cost) {
-    
+    if (cost >= COST_CRITERIA.discount) {
+      this.#benefit = discount;
+      this.calculateGiveaway(cost);
+    }
+
     return [this.#benefit, this.#giveaway];
   }
 
   calculateGiveaway(cost) {
-    
+    if (cost >= COST_CRITERIA.giveaway) {
+      this.#benefit.set("증정 이벤트", -25000);
+      this.#giveaway = "샴페인";
+    }
   }
 }
 
