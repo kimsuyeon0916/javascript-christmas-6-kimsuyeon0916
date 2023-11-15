74ebf3206f505e805cb13195852e7fe6aac2ebb0
kimsuyeon0916 <kimsuyeon_09162naver.com>
2023-11-15 14:41:31 +0900

    feat: InputView 구현 - readDate - readMenu - exceptionHandler

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,16 +1,40 @@
+import { Console } from "@woowacourse/mission-utils";
+import { MESSAGE } from "../utils/Constants.js";
+import Validation from "../utils/Validation.js";
+
 const InputView = {
   async readDate() {
-    
+    const date = await this.exceptionHandler(
+      MESSAGE.getDate,
+      Validation.checkDate
+    );
     return Number(date);
   },
 
   async readMenu() {
-    
+    const menu = await this.exceptionHandler(
+      MESSAGE.getMenu,
+      Validation.checkMenu
+    );
     return menu;
   },
 
   async exceptionHandler(message, Validate) {
-    
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      try {
+        // eslint-disable-next-line no-await-in-loop
+        const input = await Console.readLineAsync(message);
+        if (message === MESSAGE.getMenu) {
+          Validate(input.split(","));
+          return input.split(",");
+        }
+        Validate(input);
+        return input;
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
   },
 };
 
