df8342208aa281033d6d7967060cfa8923b70150
kimsuyeon0916 <kimsuyeon_09162naver.com>
2023-11-15 17:19:43 +0900

    fix: App.js 의 run 함수 내의 plan 함수 앞에 await 붙이기
    이유: 로컬에서는 실행이 잘됐지만, 테스트케이스는 통과 안되서 바로 먼저 async,await 확인 이후 오류 검출

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import EventPlanner from "./controllers/EventPlanner.js";
 class App {
   // eslint-disable-next-line class-methods-use-this
   async run() {
-    EventPlanner.plan();
+    await EventPlanner.plan();
   }
 }
 
