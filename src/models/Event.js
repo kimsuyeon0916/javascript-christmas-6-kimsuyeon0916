e8239f69e9e55fcb45b7877b6d9456b95b8f8d51
kimsuyeon0916 <kimsuyeon_09162naver.com>
2023-11-15 14:53:31 +0900

    feat: Event 구현
    - calculatePayment
    - giveBadge

--- src/models/Event.js
+++ src/models/Event.js
@@ -1,19 +1,31 @@
+import { BADGE, BADGE_CRITERIA } from "../utils/Constants.js";
+
 class Event {
   #badge;
 
   #payment;
 
   constructor() {
-    this.#badge;
+    this.#badge = BADGE.null;
   }
 
   calculatePayment(cost, totalDiscount) {
-    
+    this.#payment = cost + totalDiscount;
+
     return this.#payment;
   }
 
   giveBadge(totalBenefit) {
-    
+    if (totalBenefit <= BADGE_CRITERIA.star) {
+      this.#badge = BADGE.star;
+    }
+    if (totalBenefit <= BADGE_CRITERIA.tree) {
+      this.#badge = BADGE.tree;
+    }
+    if (totalBenefit <= BADGE_CRITERIA.santa) {
+      this.#badge = BADGE.santa;
+    }
+
     return this.#badge;
   }
 }
