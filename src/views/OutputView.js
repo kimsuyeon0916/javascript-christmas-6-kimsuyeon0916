a3dc5831da50af67a5e660bf00bc9fa40cfff0c6
kimsuyeon0916 <kimsuyeon_09162naver.com>
2023-11-15 14:45:09 +0900

    feat: OutputView 구현 - printOrder - printBenefitResult - printEventResult - printMenu - printCost - printGiveaway - printBenefit - printTotalBenefit - printPayment - printBadge

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,42 +1,62 @@
-sconst OutputView = {
+import { Console } from "@woowacourse/mission-utils";
+import { MESSAGE } from "../utils/Constants.js";
+
+const OutputView = {
   printOrder(date, order, cost) {
-    
+    Console.print(`12월 ${date}일에 ${MESSAGE.preview}`);
+    this.printMenu(order);
+    this.printCost(cost);
   },
 
   printBenefitResult(giveaway, benefit, totalbenefit) {
-    
+    this.printGiveaway(giveaway);
+    this.printBenefit(benefit);
+    this.printTotalBenefit(totalbenefit);
   },
 
   printEventResult(payment, badge) {
-    
+    this.printPayment(payment);
+    this.printBadge(badge);
   },
 
   printMenu(order) {
-    
+    Console.print(MESSAGE.menu);
+    order.forEach((count, menu) => {
+      Console.print(`${menu} ${count}개`);
+    });
   },
 
   printCost(cost) {
-    
+    Console.print(`${MESSAGE.cost}${cost.toLocaleString()}원`);
   },
 
   printGiveaway(giveaway) {
-    
+    if (giveaway.length === 0) {
+      return Console.print(`${MESSAGE.giveaway}없음`);
+    }
+    return Console.print(`${MESSAGE.giveaway}${giveaway} 1개`);
   },
 
   printBenefit(benefit) {
-    
+    Console.print(MESSAGE.benefit);
+    if (benefit.size === 0) {
+      return Console.print(`없음`);
+    }
+    return benefit.forEach((value, key) => {
+      Console.print(`${key}: ${value.toLocaleString()}원`);
+    });
   },
 
   printTotalBenefit(totalbenefit) {
-    
+    Console.print(`${MESSAGE.totalbenefit}${totalbenefit.toLocaleString()}원`);
   },
 
   printPayment(payment) {
-    
+    Console.print(`${MESSAGE.payment}${payment.toLocaleString()}원`);
   },
 
   printBadge(badge) {
-    
+    Console.print(`${MESSAGE.badge}${badge}`);
   },
 };
 
