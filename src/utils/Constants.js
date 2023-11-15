fa3a9189c7339430d690bd052806e090f3fe35bb
kimsuyeon0916 <kimsuyeon_09162naver.com>
2023-11-15 14:31:28 +0900

    feat: 상수 추가

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -4,11 +4,11 @@ const MESSAGE = Object.freeze({
     "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n",
   getMenu:
     "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n",
-  preview: "에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n",
-  menu: "\n<주문 메뉴>\n",
+  preview: "우테코 식당에서 받을 이벤트 혜택 미리 보기!\n",
+  menu: "\n<주문 메뉴>",
   cost: "\n<할인 전 총주문 금액>\n",
   giveaway: "\n<증정 메뉴>\n",
-  benefit: "\n<혜택 내역>\n",
+  benefit: "\n<혜택 내역>",
   totalbenefit: "\n<총혜택 금액>\n",
   payment: "\n<할인 후 예상 결제 금액>\n",
   badge: "\n<12월 이벤트 배지>\n",
@@ -24,19 +24,24 @@ const ERROR = Object.freeze({
 const DATE = Object.freeze({
   start: 1,
   end: 31,
+  weekday: [
+    3, 4, 5, 6, 7, 10, 11, 12, 13, 14, 17, 18, 19, 20, 21, 14, 25, 26, 27, 28,
+    31,
+  ],
+  weekend: [1, 2, 8, 9, 15, 16, 22, 23, 29, 30],
   christmas: 25,
   special: [3, 10, 17, 24, 25, 31],
 });
 
 const COST_CRITERIA = Object.freeze({
-  discount: 1,
-  giveaway: 12,
+  discount: 10000,
+  giveaway: 120000,
 });
 
 const DISCOUNT = Object.freeze({
-  year: 2023,
-  unit: 1000,
-  increase: 100,
+  year: -2023,
+  unit: -1000,
+  increase: -100,
 });
 
 const MENU = Object.freeze(
@@ -92,9 +97,9 @@ const BADGE = Object.freeze({
 });
 
 const BADGE_CRITERIA = Object.freeze({
-  star: 5000,
-  tree: 10000,
-  santa: 20000,
+  star: -5000,
+  tree: -10000,
+  santa: -20000,
 });
 
 export {
