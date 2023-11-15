52acbceb389ef9a3a61f07a4ed870d95d4efb097
kimsuyeon0916 <kimsuyeon_09162naver.com>
2023-11-15 16:54:15 +0900

    fix: InputView 예외 처리 수정

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -4,33 +4,25 @@ import Validation from "../utils/Validation.js";
 
 const InputView = {
   async readDate() {
-    const date = await this.exceptionHandler(
-      MESSAGE.getDate,
-      Validation.checkDate
-    );
-    return Number(date);
-  },
-
-  async readMenu() {
-    const menu = await this.exceptionHandler(
-      MESSAGE.getMenu,
-      Validation.checkMenu
-    );
-    return menu;
-  },
-
-  async exceptionHandler(message, Validate) {
-    // eslint-disable-next-line no-constant-condition
     while (true) {
       try {
         // eslint-disable-next-line no-await-in-loop
-        const input = await Console.readLineAsync(message);
-        if (message === MESSAGE.getMenu) {
-          Validate(input.split(","));
-          return input.split(",");
-        }
-        Validate(input);
-        return input;
+        const date = await Console.readLineAsync(MESSAGE.getDate);
+        Validation.checkDate(date);
+        return Number(date);
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+  },
+
+  async readMenu() {
+    while (true) {
+      try {
+        // eslint-disable-next-line no-await-in-loop
+        const menu = (await Console.readLineAsync(MESSAGE.getMenu)).split(",");
+        Validation.checkMenu(menu);
+        return menu;
       } catch (error) {
         Console.print(error.message);
       }
